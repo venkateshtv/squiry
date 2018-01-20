0d31c21dde3d73122d4b3a57e88be34329407cbd
Venkatesh <venkateshtv@outlook.com>
2018-01-20 19:38:27 +0530

    debug

--- app/client/__init__.py
+++ app/client/__init__.py
@@ -25,12 +25,13 @@ def external():
 
 @client_bp.route('/paymentsuccess',methods=['GET','POST'])
 def payment_success():
-    payment_gateway = PaymentFactory().get_payment_gateway(gateway)
-    payment_validated = payment_gateway.validate_payment_request(request)
-    if payment_validated['validtransaction'] == 'true':
-        return render_template('payment.html',message=payment_validated['message'])
-    else:
-        return render_template('payment.html',message=payment_validated['message'])
+    return render_template('payment.html',message=json.dumps(request))
+    # payment_gateway = PaymentFactory().get_payment_gateway(gateway)
+    # payment_validated = payment_gateway.validate_payment_request(request)
+    # if payment_validated['validtransaction'] == 'true':
+    #     return render_template('payment.html',message=payment_validated['message'])
+    # else:
+    #     return render_template('payment.html',message=payment_validated['message'])
 
 
 @client_bp.route('/paymentfailure',methods=['GET','POST'])
