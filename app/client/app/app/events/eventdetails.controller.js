571256622d87811ff2f3bc3ae04807e2c687b884
Venkatesh <venkateshtv@outlook.com>
2018-01-26 17:36:16 +0530

    Fav icon

--- app/client/app/app/events/eventdetails.controller.js
+++ app/client/app/app/events/eventdetails.controller.js
@@ -8,7 +8,7 @@
         vm.totalPrice = 0;
         vm.discounts = {};
         vm.finalPriceBeforeTax = 0;
-        vm.quantities=[{display:" ",value:0},{display:1,value:1},{display:2,value:2},{display:3,value:3},{display:4,value:4},{display:5,value:5},{display:6,value:6},{display:7,value:7},{display:8,value:8},{display:9,value:9},{display:10,value:10}];
+        vm.quantities=[{display:"0",value:0},{display:1,value:1},{display:2,value:2},{display:3,value:3},{display:4,value:4},{display:5,value:5},{display:6,value:6},{display:7,value:7},{display:8,value:8},{display:9,value:9},{display:10,value:10}];
         vm.checkout = checkout;
         vm.verifyCoupon = verifyCoupon;
         vm.discountMessage = "";        
